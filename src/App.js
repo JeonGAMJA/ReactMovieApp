d5d46170ae22d8c43fd4a78fc9bf681727c29e78
전소희 <indian_potato@naver.com>
2023-06-21 01:22:18 +0900

    Learn:EFFECTS

--- src/App.js
+++ src/App.js
@@ -5,15 +5,22 @@ function App() {
   const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
-  console.log("i run all the time");
   useEffect(() => {
-    console.log("CALL THE API.....");
+    console.log("I run only once.");
   }, []);
   useEffect(() => {
     if (keyword !== "" && keyword.length > 5) {
-      console.log("SEARCH FOR", keyword);
+      console.log("I run when 'keyword' changes.");
     }
   }, [keyword]);
+  useEffect(() => {
+    if (counter !== "") {
+      console.log("I run when 'counter' changes.");
+    }
+  }, [counter]);
+  useEffect(() => {
+    console.log("I run when 'keyword' & 'counter' changes.");
+  }, [keyword, counter]);
   return (
     <div>
       <input
