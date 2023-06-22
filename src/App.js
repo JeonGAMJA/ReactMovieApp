3577c325c3bbba8dd6f2b261e2a9e18066c4f28f
전소희 <indian_potato@naver.com>
2023-06-22 15:07:31 +0900

    Learn:Movie App

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ function App() {
   const [movies, setMovies] = useState([]);
   const getMovies = async () => {
     const response = await fetch(`https://yts.mx/api
-  https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
-      .then((response) => response.json())
-      .then((json) => {
-        setMovies(json.data.movies);
-        setLoading(false);
-      });
+  https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`);
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
   };
   useEffect(() => {
     getMovies();
